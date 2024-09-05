44fe058dc0ef0bab98aec2cc44fdf6b04c63bcb3
Bicky Eric Kantona <bickyeric@proton.me>
2024-09-05 21:37:57 +0700

    init commit

--- matcher.go
+++ matcher.go
@@ -1,3 +1,27 @@
 package gomatcher
 
+import (
+	"reflect"
+
+	"go.uber.org/mock/gomock"
+)
+
 type StructIncludes map[string]any
+
+func (m StructIncludes) Matches(x any) bool {
+	xrv := reflect.ValueOf(x)
+
+	for key, value := range m {
+		xFieldrv := xrv.FieldByName(key)
+
+		if !gomock.Eq(xFieldrv.Interface()).Matches(value) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (m StructIncludes) String() string {
+	return "apanih"
+}
